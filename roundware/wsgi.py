c79226e64712616e740242d9cb7a14fd154df1a0
Varvara <33721208+VarvaraGo@users.noreply.github.com>
2026-04-22 18:08:27 +0100

    Update wsgi.py
    11

--- roundware/wsgi.py
+++ roundware/wsgi.py
@@ -1,17 +1,5 @@
 import os
-import sys
-
-PROJECT_ROOT = '/var/www/roundware/source/'
-CODE_ROOT = '/var/www/roundware/source/roundware/'
-SETTINGS_ROOT = '/var/www/roundware/settings/'
-
-sys.stdout = sys.stderr
-
-sys.path.append(PROJECT_ROOT)
-sys.path.append(CODE_ROOT)
-sys.path.append(SETTINGS_ROOT)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'roundware.settings.dev'
-
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roundware.settings.common')
+application = get_wsgi_application()
